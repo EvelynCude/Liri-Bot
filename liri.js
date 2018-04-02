778c1506dd4232043f702d66283d22af51fc072f
EvelynCude <ebenitez@email.arizona.edu>
2018-04-01 18:35:07 -0700

    Added Liri Spotify function

--- liri.js
+++ liri.js
@@ -2,13 +2,13 @@
 var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
 var request = require("request");
-var Spotify = require("spotify");
+var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
 var fs = require("fs");
 
 
 
-//var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 
@@ -16,9 +16,10 @@ var client = new Twitter(keys.twitter);
 var nodeArgs = process.argv;
 // Create a variable to store the Liri command that was chosen.  
 var liriCommand = process.argv[2];
+
 // Create an empty variable for holding the user's search which the user inputs after the Liri command.
 var userSearch = "";
-// Loop through all the words in the node argument to handle the inclusion of "+"s
+    // Loop through all the words in the node argument to handle the inclusion of "+"s
 for (var i = 3; i < nodeArgs.length; i++) {
     if (i > 3 && i < nodeArgs.length) {
         userSearch = userSearch + "+" + nodeArgs[i];
@@ -27,10 +28,18 @@ for (var i = 3; i < nodeArgs.length; i++) {
         userSearch += nodeArgs[i];
     }
 }
-//  Liri can take four commands.  Determing which command was used:
+
+//  Liri can take four commands.  Determing which command was used and call the matching function:
 //  my-tweets
 if (liriCommand === "my-tweets"){
     showTweets();
+}else if (liriCommand === "spotify-this-song"){
+    showSpotify(userSearch);
+}else if (liriCommand === "movie-this"){
+    showMovie(userSearch);
+}else if (liriCommand === "do-what-it-says"){
+    showRandom();
+}
 
 //  node liri.js my-tweets
 //  This will show your last 20 tweets and when they were created at in your terminal / bash window.
@@ -41,19 +50,51 @@ function showTweets() {
         // console log tweets (pretty)
         if (!error){
             var tweetList = 
-            '\n------------------------------------------------------------------------------------\n\n' 
-            + 'Last 20 Tweets:\n\n' + 
-            '------------------------------------------------------------------------------------\n';
+            "\n------------------------------------------------------------------------------------\n\n" 
+            + "Last 20 Tweets:\n\n" + 
+            "------------------------------------------------------------------------------------\n";
 
             for (var i = 0; i < tweets.length; i++) {
-                tweetList += 'Tweeted on: ' + tweets[i].created_at + '\n' +
-                    'Tweet: ' + tweets[i].text + '\n' +
-                    'Retweet Count: ' + tweets[i].retweet_count + '\n' +
-                    'Favorite Count: ' + tweets[i].favorite_count + '\n' +
-                    '--------------------------------------------\n';
+                tweetList += "Tweet: " + tweets[i].text + "\n\n" +
+                    "Tweeted on: " + tweets[i].created_at + "\n" +
+                    "--------------------------------------------\n";
             }
             console.log(tweetList);
         }
+    });
+}
+
+//  node liri.js spotify-this-song '<song name here>'
+//  This will show the following information about the song in your terminal / bash window
+//      Artist(s)
+//      The song's name
+//      A preview link of the song from Spotify
+//      The album that the song is from
+function showSpotify(userSearch){
+    var song;
+    if (userSearch === ""){
+        song = "The Sign Ace of Base";
+    }else {
+        song = userSearch;
+    }
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        //console.log(data);
+        //console.log(data.tracks.items[0]);
+
+        var songData = data.tracks.items[0];
+        var songInfo = 
+            "\n----------------------------------------------------------------\n"
+            + "Song Information:\n" 
+            + "----------------------------------------------------------------\n"
+            + "Artist(s): " + songData.artists[0].name + "\n" 
+            + "Song Name: " + songData.name + "\n"
+            + "Preview Link: " + songData.preview_url + "\n"
+            + "Album: " + songData.album.name + "\n"
+            + "----------------------------------------------------------------\n";
+        console.log(songInfo);
     });
 }
 
@@ -75,4 +116,3 @@ function showTweets() {
 //         // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
 //         console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
 //     }
- };
